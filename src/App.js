803ba7c677ce26b23d76e7931623b8fc75b2d0f3
Kofi <kofi@ymail.com>
2023-04-15 14:18:41 +0300

    New update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ModalPage from "./pages/ModalPage";
 import TablePage from "./pages/TablePage";
 import CounterPage from "./pages/CounterPage";
 
+ //updated
 function App() {
   return (
     <div className="container mx=auto grid grid-cols-6 gap-4 mt-4">
